de8e052ec29b4b8d0d560dd48b13b58d76122f79
Bobby Galli <bobbyg603@gmail.com>
2021-05-06 10:15:47 -0400

    Only upload Plugin symbols in PostBuild

--- Editor/PostBuild.cs
+++ Editor/PostBuild.cs
@@ -1,23 +1,18 @@
-﻿using System.IO;
-using UnityEngine;
+﻿using System.Diagnostics;
+using System.IO;
 using UnityEditor;
 using UnityEditor.Callbacks;
-using System.Diagnostics;
-using System.Collections.Generic;
-using System;
-using System.Threading.Tasks;
-using System.Threading;
-using System.Net.Http;
+using UnityEngine;
 
 public class BuildPostprocessor
 {
-    const string UNITY_ASSEMBLY_FILENAME = "UnityEngine.dll";
-    const string UNITY_ASSEMBLY_MDB_FILENAME = UNITY_ASSEMBLY_FILENAME + ".mdb";
-    const string MAIN_ASSEMBLY_FILENAME = "Assembly-CSharp.dll";
-    const string MAIN_ASSEMBLY_MDB_NAME_FILENAME = MAIN_ASSEMBLY_FILENAME + ".mdb";
-
     static string _platform;
 
+    /// <summary>
+    /// Upload Asset/Plugin symbol files to BugSplat. 
+    /// We don't upload Unity symbol files because the build output only contains public symbol information.
+    /// BugSplat is configured to use the Unity symbol server which has private symbols containing file, function, and line information.
+    /// </summary>
     [PostProcessBuild(1)]
     public static void OnPostprocessBuild(BuildTarget target, string pathToBuiltProject)
     {
@@ -30,77 +25,25 @@ public class BuildPostprocessor
 
         if (!string.IsNullOrEmpty(_platform))
         {
-            var builtProjectDir = Path.GetDirectoryName(pathToBuiltProject);
             var projectDir = Path.GetDirectoryName(Application.dataPath);
-            var tempSymbolsDir = Path.Combine(builtProjectDir, "Symbols");
             var bugSplatDir = Path.Combine(projectDir, @"..\..\bin");
+            var pluginsDir = Path.Combine(Path.Combine(projectDir, @"Assets\Plugins"), _platform);
 
-            if (!Directory.Exists(tempSymbolsDir))
-            {
-                Directory.CreateDirectory(tempSymbolsDir);
-            }
-
-            CopyPluginSymbols(projectDir, tempSymbolsDir);
-            CopyRuntimeSymbols(builtProjectDir, tempSymbolsDir);
-            RunSendPdbs(builtProjectDir, bugSplatDir);
-
-            Directory.Delete(tempSymbolsDir, true);
+            RunSendPdbs(pluginsDir, bugSplatDir);
         }
     }
 
-    static void CopyPluginSymbols(string projectDir, string symbolsDir)
-    {
-        var pluginsDir = Path.Combine(Path.Combine(projectDir, @"Assets\Plugins"), _platform);
-
-        if (!Directory.Exists(pluginsDir))
-        {
-            UnityEngine.Debug.Log($"Directory {pluginsDir} does not exist, skipping...");
-            return;
-        }
-
-        var pluginFilePaths = new List<string>();
-        pluginFilePaths.AddRange(Directory.GetFiles(pluginsDir, "*.pdb", SearchOption.AllDirectories));
-        pluginFilePaths.AddRange(Directory.GetFiles(pluginsDir, "*.dll", SearchOption.AllDirectories));
-
-        foreach (var pluginFilePath in pluginFilePaths)
-        {
-            var fileName = Path.GetFileName(pluginFilePath);
-            var dstFilePath = Path.Combine(symbolsDir, fileName);
-
-            File.Copy(pluginFilePath, dstFilePath, true);
-        }
-    }
-
-    static void CopyRuntimeSymbols(string builtProjectDir, string symbolsDir)
-    {
-        var dataDir = Path.Combine(builtProjectDir, string.Format("{0}_Data", Application.productName));
-        var managedDllsDir = Path.Combine(dataDir, "Managed");
-        // TODO BG all dlls? (except maybe system dlls?)
-        // TODO BG mono dlls and pdbs!
-        var mainAssembly = Path.Combine(managedDllsDir, MAIN_ASSEMBLY_FILENAME);
-        var unityAssembly = Path.Combine(managedDllsDir, UNITY_ASSEMBLY_FILENAME);
-        var unityMdb = Path.Combine(managedDllsDir, UNITY_ASSEMBLY_MDB_FILENAME);
-
-        File.Copy(mainAssembly, Path.Combine(symbolsDir, MAIN_ASSEMBLY_FILENAME), true);
-        File.Copy(unityAssembly, Path.Combine(symbolsDir, UNITY_ASSEMBLY_FILENAME), true);
-
-        if (File.Exists(unityMdb))
-        {
-            File.Copy(unityMdb, Path.Combine(symbolsDir, UNITY_ASSEMBLY_MDB_FILENAME), true);
-        }
-    }
-
-    static void RunSendPdbs(string builtProjectDir, string bugSplatDir)
+    static void RunSendPdbs(string pluginsDir, string bugSplatDir)
     {
         var sendPdbsPath = Path.Combine(bugSplatDir, "SendPdbs.exe");
         var startInfo = new ProcessStartInfo(sendPdbsPath);
-        startInfo.Arguments = "/u Fred"
+        startInfo.Arguments = "/u fred@bugsplat.com"
             + " /p Flintstone"
             + " /b Fred"
             + " /a " + Application.productName
             + " /v " + Application.version
-            + " /d \"" + builtProjectDir + "\""
-            + " /f \"Symbols/*;*.exe;*.pdb";
+            + " /d \"" + pluginsDir + "\""
+            + " /s";
         using (var process = Process.Start(startInfo))
         {
             process.WaitForExit();
