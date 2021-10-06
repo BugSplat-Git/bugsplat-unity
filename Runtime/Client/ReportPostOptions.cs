1367bc4435843c5b4fd3adfd47add180d91bce70
Bobby Galli <bobbyg603@gmail.com>
2021-10-06 16:37:43 -0400

    Moves tightly coupled interfaces into files of classes they describe

--- Runtime/Client/ReportPostOptions.cs
+++ Runtime/Client/ReportPostOptions.cs
@@ -2,9 +2,28 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
 
 namespace BugSplatUnity.Runtime.Client
 {
+    public interface IFormDataParam
+    {
+        public string Name { get; set; }
+        public HttpContent Content { get; set; }
+        public string FileName { get; set; }
+    }
+
+    public interface IReportPostOptions
+    {
+        public List<FileInfo> AdditionalAttachments { get; }
+        public List<IFormDataParam> AdditionalFormDataParams { get; }
+        public string Description { get; set; }
+        public string Email { get; set; }
+        public string Key { get; set; }
+        public string User { get; set; }
+        public int CrashTypeId { get; set; }
+    }
+
     public class ReportPostOptions : IReportPostOptions
     {
         public List<FileInfo> AdditionalAttachments { get; }
--- Runtime/Client/UnityWebClient.cs
+++ Runtime/Client/UnityWebClient.cs
@@ -1,14 +1,35 @@
-﻿using BugSplatDotNetStandard;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.Networking;
+using static UnityEngine.Networking.UnityWebRequest;
 
 namespace BugSplatUnity.Runtime.Client
 {
+    internal interface IDownloadHandler
+    {
+        string text { get; }
+    }
+
+    internal interface IUnityWebRequest
+    {
+        IEnumerator SendWebRequest();
+        Result result { get; }
+        string error { get; }
+        long responseCode { get; }
+        IDownloadHandler downloadHandler { get; }
+    }
+
+    internal interface IUnityWebClient
+    {
+        IReportPostOptions CreateExceptionPostOptions();
+        IUnityWebRequest Post(string url, Dictionary<string, string> formData);
+    }
+
     internal class UnityWebClient : IUnityWebClient
     {
         public IReportPostOptions CreateExceptionPostOptions()
         {
-            return (IReportPostOptions)new ReportPostOptions();
+            return new ReportPostOptions();
         }
 
         public IUnityWebRequest Post(string url, Dictionary<string, string> formData)
