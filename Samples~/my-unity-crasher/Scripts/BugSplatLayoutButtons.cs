094f93d3e2547df9d5cc5ac715efdf161a97d182
ZanderLouie <zander@bugsplat.com>
2021-12-03 11:54:01 -0700

    Automate button colors/force orientation/Add better platform exclusion tech

---
+++ Samples~/my-unity-crasher/Scripts/BugSplatLayoutButtons.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteInEditMode]
+public class BugSplatLayoutButtons : MonoBehaviour
+{
+    private readonly Color bugSplatRed = new Color32(244, 102, 137, 255);
+    private readonly Color bugSplatGreen = new Color32(74, 235, 195, 255);
+    private readonly Color bugSplatBlue = new Color32(58, 163, 255, 255);
+    
+
+    void Awake()
+    {
+        var images = transform.GetComponentsInChildren<Image>();
+        var count = 0;
+        foreach (var image in images)
+        {            
+            if (image.gameObject.activeInHierarchy)
+            {
+                Debug.Log(count);
+                switch (count % 3)
+                {
+                    case 0:
+                        image.color = bugSplatRed;
+                        break;
+                    case 1:
+                        image.color = bugSplatBlue;
+                        break;
+                    case 2:
+                        image.color = bugSplatGreen;
+                        break;
+                }
+                count++;
+            }
+        }
+
+    }
+}
